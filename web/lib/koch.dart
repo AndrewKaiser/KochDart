bbf4e22fcfbf7ba7349c6bfa08d1f2fe3d0378fb
AndrewKaiser <kaisea@rpi.edu>
2015-01-12 17:13:50 -0500

    it worksgit add --all! any line fractal can be made now

--- web/lib/koch.dart
+++ web/lib/koch.dart
@@ -2,57 +2,153 @@ import 'dart:html';
 import 'dart:math';
 import 'mouse.dart';
 
-class Position {
-  Point pos;
-  Mouse mouse;
-  
-  Position(this.mouse, this.pos);
-  
-  int x() {return x;}
-  int y() {return y;}
-  void draw(CanvasRenderingContext2D context) {
-    
-  }
-}
 
 class Shape {
   CanvasElement canvas;
   num width;
   num height;
+  bool useMouse = true;
   Mouse mouse;
   List<Point> points = <Point>[];
+  String id = 'copy';
+  var onMouseDown;
+  var requestId;
+
   
-  Shape.Original(this.canvas);
+  Shape.Original(this.canvas) {
+    points.clear();
+    id = 'original';
+  }
   Shape.List(this.points, this.canvas);
   Shape.Copy(Shape old, this.canvas) {
+    points.clear();
+//    this.canvas  = old.canvas;
+    width = old.width;
+    height = old.height;
     points.addAll(old.points);
+    useMouse = false;
+    onMouseDown = null;
+    mouse = null;
+    
   }
-  
+
   start() {
     width = canvas.width;
     height = canvas.height;
     mouse = new Mouse(canvas);
+    useMouse = true;
     
+    onMouseDown = canvas.onMouseDown.listen(addPoint);
     requestRedraw();
+  }
+  startFractal() {
+    window.cancelAnimationFrame(requestId);
+    useMouse = false;
+    mouse = null;
+    onMouseDown.cancel();
+  }
+  render([CanvasRenderingContext2D context]) {
+    if (points.isEmpty) return;
+    var context = canvas.context2D;
+
+    context..lineWidth = 0.5
+           ..strokeStyle = 'black';
+//    print("$id: $points");
+    context..beginPath()
+           ..moveTo(points.first.x, points.first.y);
+    for (int i=1; i<this.size(); ++i) {
+           context.lineTo(points[i].x, points[i].y);
+    }
+           context.stroke();
   }
   int size() {
     int length = 0;
-    for(var p in points) length ++;
+    for(var p in points) {
+      length ++;
+    }
     return length;
+  }
+  num getAngle(num x, num y) {
+    if (x > 0) return atan(y/x);
+    if (x < 0 && y >= 0) return atan(y/x) + PI;
+    if (x < 0 && y < 0) return atan(y/x) - PI;
+    //    if x = 0 then
+    if (y > 0) return PI/2;
+    if (y < 0) return -PI/2;
+    if (y == 0) return null;
+  }
+  double getBaseDistance() {
+    return sqrt(pow(points.first.x-points.last.x,2) + pow(points.first.y-points.last.y,2));
   }
   Point getPoint(int i) {
     return points[i];
   }
   void addPoint(MouseEvent event) {
+    if (event.button == 2) return;
     points.add(new Point(event.client.x, event.client.y));
+  }
+  void translate(int i) {
+    num dx = points[i].x - points.first.x;
+    num dy = points[i].y - points.first.y;
+    for (int w = 0; w < this.size(); ++w) {
+      points[w] = new Point(points[w].x+dx, points[w].y+dy);
+    }
+  }
+  void rotate(int i, angle) {
+//    num DX = points[i+1].x - points[i].x; 
+//    num DY = points[i+1].y - points[i].y;
+//    num DX = points[i].x - points[i+1].x; 
+//    num DY = points[i].y - points[i+1].y;
+//    var newAngle = getAngle(DX,DY);
+//    print("($DX, $DY) angle: $newAngle");
+//    DX = points.last.x - points.first.x;
+//    DY = points.last.x - points.first.y;
+//    DX = points.first.x - points.last.x;
+//    DY = points.first.y - points.last.x;
+//    var oAngle = getAngle(DX,DY);
+//    var newAngle = acos(DX/sqrt(pow(DX,2) + pow(DY,2))) - PI;
+    
+    
+//    num angle = (oAngle - newAngle);
+//    print("final: $angle, oAngle: $oAngle, newAngle: $newAngle");
+    // the point which the others in lines will use as an origin
+    
+    num cosTheta = cos(angle);
+    num sinTheta = sin(angle);
+//    print("cos: $cosTheta, sin: $sinTheta");
+    // actually rotating all the points
+    for (int w=1; w<size(); ++w) {
+      num x = cosTheta*(points[w].x-points.first.x) - sinTheta*(points[w].y-points.first.y) + points.first.x;
+      num y = sinTheta*(points[w].x-points.first.x) + cosTheta*(points[w].y-points.first.y) + points.first.y;
+      points[w] = new Point(x, y);
+    }
+
+  }
+  void dialate(int i, double levelAbove) {
+    var currentDist = sqrt(pow(points[i].x-points[i+1].x,2)
+                         + pow(points[i].y-points[i+1].y,2));
+
+    double ratio = (levelAbove-currentDist)/levelAbove;
+    num pivotX = points.first.x;
+    num pivotY = points.first.y;
+    for (int w=1; w<size(); ++w) {
+      num shrinkX = points[w].x;
+      num shrinkY = points[w].y;
+      points[w] = new Point(shrinkX - (shrinkX - pivotX)*ratio, 
+                            shrinkY - (shrinkY - pivotY)*ratio);
+        // PVector translate(pivotX-);
+    }
+
   }
   void draw([_]) {
     var context = canvas.context2D;
     drawBackground(context);
     drawLine(context);
     
-    canvas.onMouseDown.listen(addPoint);
-    requestRedraw();
+    
+    if (useMouse) {
+      requestRedraw();
+    }
   }
   void drawBackground(CanvasRenderingContext2D context) {
     context.clearRect(0, 0, width, height);
@@ -71,14 +167,15 @@ class Shape {
              context.stroke();
     }
 
-    
-    context..beginPath()
-           ..moveTo(points.last.x, points.last.y)
-           ..lineTo(mouse.x, mouse.y)
-//           ..closePath()
-           ..stroke();
+    if (useMouse) {
+      context..beginPath()
+             ..moveTo(points.last.x, points.last.y)
+             ..lineTo(mouse.x, mouse.y)
+  //           ..closePath()
+             ..stroke();
+    }
   }
   void requestRedraw() {
-    window.requestAnimationFrame(draw);
+    requestId = window.requestAnimationFrame(draw);
   }
 }
--- web/main.dart
+++ web/main.dart
@@ -2,24 +2,76 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math';
 import 'lib/koch.dart';
 
+int maxDepth = 0;
+Shape original;
+List<num> oAngles;
+
 void main() {
-  
   CanvasElement canvas = querySelector('#koch');
-//  setColor(canvas, 'black');
-  Shape original = new Shape.Original(canvas);
+  CanvasRenderingContext2D context = canvas.getContext('2d');
+  original  = new Shape.Original(canvas);
   original.start();
-  canvas.onKeyDown.listen(increaseDepth);
+  canvas.onContextMenu.listen((MouseEvent e) => e.preventDefault());
+  canvas.onMouseDown.listen(increaseDepth);
+  canvas.onKeyDown.listen(startOver);
 }
 void setColor(CanvasElement canvas, String color) {
   CanvasRenderingContext2D context = canvas.getContext('2d');
   context.fillStyle = color;
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
-void increaseDepth(KeyboardEvent key) {
-  
+void findAngles(Shape original) {
+  oAngles = <num>[];
+  for(int i=0; i<original.size()-1; ++i) {
+    num x = original.points[i].x - original.points[i+1].x;
+    num y = original.points[i].y - original.points[i+1].y;
+    num sideAngle = 2*atan((sqrt(pow(x,2) + pow(y,2)) - x)/y);
+    
+    x = original.points.first.x - original.points.last.x;
+    y = original.points.first.y - original.points.last.y;
+    num oAngle = 2*atan((sqrt(pow(x,2) + pow(y,2)) - x)/y);
+   
+    oAngles.add((oAngle - sideAngle)*-1);
+  }
+}
+void startOver(KeyboardEvent key) {
+  print('here');
+  print(key);
+}
+void increaseDepth(MouseEvent event) {
+  if (event.button != 2) return;
+  if (original.size() < 2) return;
+  original.startFractal();
+//  print("oAngle: $oAngle");
+  maxDepth ++;
+//  print(original.getAngle());
+//  original.size();
+  CanvasElement canvas = querySelector('#koch');
+  var context = canvas.context2D;
+  original.drawBackground(context);
+  findAngles(original);
+  fractalize(original, 0);
+  print('done');
 }
 void fractalize(Shape current, int depth) {
-  
+  CanvasElement canvas = querySelector("#koch");
+  if (depth == maxDepth) {
+    //    original.drawBackground(context);
+    current.render();
+    return;
+  }
+  var levelAbove = current.getBaseDistance();
+  for (int i=0; i<current.size() - 1; ++i) {
+//    Shape newDepth = new Shape.Copy(current, canvas);
+    Shape newDepth = new Shape.Copy(current, canvas);
+    newDepth.translate(i);
+//    print(current.points);
+    newDepth.rotate(i, oAngles[i]);
+    newDepth.dialate(i, levelAbove);
+    fractalize(newDepth, depth + 1);
+//    print('going');
+  }
 }
